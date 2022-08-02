074dcdfb4f6bedc9c15937849dcd278d7894727c
falcaomm <manu.m.falcao@gmail.com>
2022-08-02 20:17:53 -0300

    Modificações feedback

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,10 +1,10 @@
 const sacolao = []
 
 console.log('Sacolão vazio:', sacolao);
- 
-item1 = {nome: "Banana", precoKg: "2,49", disponibilidade: true}
-item2 = {nome: "Carambola", precoKg: "2,99", disponibilidade: false}
-item3 = {nome: "Abacaxi", precoKg: "3,99", disponibilidade: true}
+
+item1 = { nome: "Banana", precoKg: "2,49", disponibilidade: true }
+item2 = { nome: "Carambola", precoKg: "2,99", disponibilidade: false }
+item3 = { nome: "Abacaxi", precoKg: "3,99", disponibilidade: true }
 
 sacolao.push(item1, item2, item3)
 
